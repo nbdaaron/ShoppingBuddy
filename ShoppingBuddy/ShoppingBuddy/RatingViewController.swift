27368eb86345960120529f2892bf48811ad12af2
Aaron Kau <aaron.kau@rutgers.edu>
2017-04-15 19:52:46 -0400

    Finished

--- ShoppingBuddy/ShoppingBuddy/RatingViewController.swift
+++ ShoppingBuddy/ShoppingBuddy/RatingViewController.swift
@@ -8,13 +8,89 @@
 
 import UIKit
 import Cosmos
+import Firebase
+import FirebaseStorage
 
-class RatingViewController: UIViewController {
+class RatingViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     @IBOutlet weak var rating: CosmosView!
+    @IBOutlet weak var imageView: UIImageView!
         
+    @IBOutlet weak var comment: UITextField!
+    
+    @IBAction func submit(_ sender: Any) {
+        
+        let uid = FIRAuth.auth()!.currentUser!.uid
+        let users = FIRDatabase.database().reference().child("users")
+        //let storage = FIRStorage.storage().reference()
+        
+        
+        users.child(uid).child("rating").setValue(rating.rating)
+        users.child(uid).child("comment").setValue(comment.text!)
+        
+        users.child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            let partnerid = value?["partner"] as! String
+            
+            //print("Parter value: \(partnerid)")
+            
+            self.spinner.startAnimating()
+            
+            users.child(partnerid).child("rating").observe(FIRDataEventType.value, with: { (snapshot) in
+                let value = snapshot.value as? Double
+                print("Value: \(value)")
+                if value != nil {
+                    users.child(partnerid).child("rating").removeAllObservers()
+                    self.performSegue(withIdentifier: "results", sender: nil)
+                    print("Both Ratings.")
+                    self.spinner.stopAnimating()
+                    //FIRDatabase.database().reference().child("queue").child(uid).removeValue()
+                }
+            })
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        spinner.hidesWhenStopped = true
+        comment.delegate = self
+        
+        let uid = FIRAuth.auth()!.currentUser!.uid
+        let databaseRef = FIRDatabase.database().reference()
+        let storageRef = FIRStorage.storage().reference()
+        let users = FIRDatabase.database().reference().child("users")
+        
+        users.child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            let partnerid = value?["partner"] as! String
+            
+            databaseRef.child("users").child(partnerid).observeSingleEvent(of: .value, with: { (snapshot) in
+                // check if user has photo
+                if snapshot.hasChild("image"){
+                    // set image locatin
+                    let filePath = "\(partnerid)/\("userPhoto")"
+                    // Assuming a < 10MB file, though you can change that
+                    storageRef.child(filePath).data(withMaxSize: 10*1024*1024, completion: { (data, error) in
+                        
+                        let userPhoto = UIImage(data: data!)
+                        self.imageView.image = userPhoto
+                        print("loaded photo")
+                    })
+                }
+            })
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+
+        
+
         // Do any additional setup after loading the view.
     }
     
@@ -24,7 +100,14 @@ class RatingViewController: UIViewController {
     }
     
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
     /*
      // MARK: - Navigation
