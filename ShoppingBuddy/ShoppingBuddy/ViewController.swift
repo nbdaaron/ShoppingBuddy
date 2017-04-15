7103523fd7a0712121bcc72a97df05f1c0f6754b
Aaron Kau <aaron.kau@rutgers.edu>
2017-04-15 13:57:21 -0400

    Initial Commit

---
+++ ShoppingBuddy/ShoppingBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppingBuddy
+//
+//  Created by Aaron Kau on 4/15/17.
+//  Copyright © 2017 Aaron Kau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
